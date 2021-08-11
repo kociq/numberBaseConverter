394b99f00a6820edda451a87729bcc0e6fefad4c
Katarzyna Sękowska <84277892+kociq@users.noreply.github.com>
2021-08-11 02:27:15 +0200

    added converting from base 10 to any other

--- Main.java
+++ Main.java
@@ -2,16 +2,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter number in decimal system:");
-        Scanner scanner = new Scanner(System.in);
-        int baseTenNumber = scanner.nextInt();
-        System.out.println("Enter target base:");
-        int targetBase = scanner.nextInt();
-        System.out.println("Convertion result: " + convertToBase(baseTenNumber, targetBase));
-
+        boolean exit = false;
+        do {
+            System.out.println("Do you want to convert /from decimal or /to decimal? (To quit type /exit)");
+            Scanner scanner = new Scanner(System.in);
+            String move = scanner.nextLine();
+            switch (move) {
+                case "/from":
+                    System.out.println("Enter a number in decimal system:");
+                    int baseTenNumber = scanner.nextInt();
+                    System.out.println("Enter target base:");
+                    int targetBase = scanner.nextInt();
+                    System.out.println("Conversion result: " + convertBaseTenToAny(baseTenNumber, targetBase));
+                    break;
+                case "/to":
+                    System.out.println("Enter source number:");
+                    String anyBaseNumber = scanner.nextLine();
+                    System.out.println("Enter source base:");
+                    int sourceBase = scanner.nextInt();
+                    System.out.println("Conversion to decimal result: " + convertBaseAnyToTen(anyBaseNumber, sourceBase));
+                    break;
+                case "/exit":
+                    exit = true;
+                    break;
+            }
+        } while (!exit);
     }
     
-    public static String convertToBase(int baseTenNumber, int base) {
+    public static String convertBaseTenToAny(int baseTenNumber, int base) {
         String result = "";
         int remainder = 0;
         do {
@@ -19,23 +37,34 @@ public class Main {
             baseTenNumber /= base;
             switch (remainder) {
                 case 10:
-                    result = "A".concat(result);
+                    result = "a".concat(result);
+                    break;
                 case 11:
-                    result = "B".concat(result);
+                    result = "b".concat(result);
+                    break;
                 case 12:
-                    result = "C".concat(result);
+                    result = "c".concat(result);
+                    break;
                 case 13:
-                    result = "D".concat(result);
+                    result = "d".concat(result);
+                    break;
                 case 14:
-                    result = "E".concat(result);
+                    result = "e".concat(result);
+                    break;
                 case 15:
-                    result = "F".concat(result);
+                    result = "f".concat(result);
+                    break;
                 default:
                     result = Integer.toString(remainder).concat(result);
             }
         } while (baseTenNumber > 0);
 
         return result;
+    }
+
+    public static int convertBaseAnyToTen(String anyBaseNumber, int base) {
+        return Integer.parseInt(anyBaseNumber, base);
+
     }
 
 }
